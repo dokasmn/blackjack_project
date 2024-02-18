79d47a0db3156bf277d112ed5a77c2d2a8222a87
Cauan de Souza Moreira <novocsm@gmail.com>
2024-02-18 01:06:47 -0300

    update

---
+++ teste/System/projeto/blackjack_jogo/static/js/scripts.js
@@ -0,0 +1,1 @@
+console.log("blackjack deu bom!");
--- teste/System/venv/Scripts/activate.bat
+++ teste/System/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Users\cauan_moreira\Documents\GitHub\blackjack_project\System\venv
+set VIRTUAL_ENV=C:\Users\novoc\OneDrive\Documentos\GitHub\blackjack_project\fork\teste\System\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
